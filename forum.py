5db9a81c740d2acac277adec440e01d483f46828
geovane186 <jdp.gsr@gmail.com>
2021-11-23 01:31:25 -0300

    Update forum.py

--- forum.py
+++ forum.py
@@ -21,6 +21,104 @@ from os import putenv
 import subprocess
 import sys
 
+class User:
+	def __init__(self):
+		self.password = ''
+		self.publicKey = ''
+		self.privateKey = ''
+		self.reputation = ''
+	
+	#Getter
+	def getPassword(self):
+		return self.password
+	
+	def getPublicKey(self):
+		return self.publicKey
+	
+	def getPrivateKey(self):
+		return self.privateKey
+	
+	def getReputation(self):
+		return self.reputation
+	
+	#Setter
+	def setPassword(self, value):
+		self.password = value
+	
+	def setPublicKey(self, value):
+		self.publicKey = value
+	
+	def setPrivateKey(self, value):
+		self.privateKey = value
+		
+	def setReputation(self, value):
+		self.reputation = value
+	
+class Post:
+	def __init__(self):
+		self.fileName = ''
+		self.hash = ''
+		self.privateKey = ''
+		self.reputation = ''
+	
+	#Getter
+	def getFileName(self):
+		return self.fileName
+	
+	def getFilePath(self):
+		return self.filePath
+	
+	def getHash(self):
+		return self.hash
+	
+	def getPrivateKey(self):
+		return self.privateKey
+	
+	def getReputation(self):
+		return self.reputation
+	
+	#Setter
+	def setFileName(self, value):
+		self.fileName = value
+	
+	def setFilePath(self, value):
+		self.filePath = value
+	
+	def setHash(self, value):
+		self.hash = value
+	
+	def setPrivateKey(self, value):
+		self.privateKey = value
+		
+	def setReputation(self, value):
+		self.reputation = value
+
+class Chain:
+	def __init__(self):
+		self.name = ''
+		self.hash = ''
+		self.pionerKey = ''
+	
+	#Getter
+	def getName(self):
+		return self.name
+	
+	def getHash(self):
+		return self.hash
+	
+	def getPionerKey(self):
+		return self.pionerKey
+	
+	#Setter
+	def setName(self, value):
+		self.name = value
+	
+	def setHash(self, value):
+		self.hash = value
+	
+	def setPionerKey(self, value):
+		self.pionerKey = value
+
 """ def checkKey(privateKey,publicKey):
 	if privateKey != '' and publicKey != '':
 		return True
@@ -94,8 +192,11 @@ def likePost(chainName, post, privateKey, msg):
 
 	return freeExec.stdout
 
+def isPost(chainName, hash):
+	res = getBlockInfo(chainName,hash)
+	print(res)
+
 def listPosts(chainName):
-	print(chainName)
 	freeExec = subprocess.run(
 		['freechains', 'chain', '#'+str(chainName), 'heads'], capture_output=True, text=True
 	)
@@ -105,7 +206,8 @@ def listPosts(chainName):
 	ps = freeExec.stdout.split(' ')
 	listPosts = []
 	for i in ps:
-		listPosts.append(i)
+		if isPost(chainName, i):
+			listPosts.append(i)
 
 	return listPosts
 
@@ -168,14 +270,26 @@ def recvChainHost(chainName, hostIp, hostPort, origIp, origPort):
 
 	return freeExec.stdout
 
-def updatePostList(newsPosts, posts):
-	for post in newsPosts:
-		newPost = (post, '', '')
-		if newPost in posts:
-			continue
-		posts.append(newPost)
+def getBlockInfo(chainName, hashBlock):
+	freeExec = subprocess.run(
+		['freechains','chain', "#"+str(chainName), 'get', 'block', hashBlock], capture_output=True, text=True
+	)
+	if freeExec.stderr:
+		print("stderr:", freeExec.stderr)
 
-def updateBlockedPostList():
+	return freeExec.stdout
+
+def updatePostList(newsPosts, posts):
+	for newPost in newsPosts:
+		p = Post()
+		p.setHash(newPost)
+		#newPost = (post, '', '')
+		for post in posts:
+			if p.getHash() == post.getHash():
+				continue
+		posts.append(p)
+
+def updateBlockedPostList(chainName):
 	newBlockedPosts = listBlockedPosts(chainName)
 	blockedPosts = []
 	for post in newBlockedPosts:
@@ -184,7 +298,7 @@ def updateBlockedPostList():
 			continue
 		blockedPosts.append(blockedPost)
 
-def print_menu(menuType, posts=None, chainName=None, blockedPosts=None):
+def print_menu(menuType, posts=None, chainName=None, blockedPosts=None, publicKey=None):
 	if menuType == 1:
 		print('Bem vindo ao Forum.\n')
 		
@@ -194,6 +308,8 @@ def print_menu(menuType, posts=None, chainName=None, blockedPosts=None):
 	
 	if menuType == 2:
 		print('\nForum Responde Ai.\n')
+
+		print('User: '+ str(publicKey)+'\n')
 		
 		print('Selecione uma opção no menu abaixo:\n')
 		
@@ -201,6 +317,8 @@ def print_menu(menuType, posts=None, chainName=None, blockedPosts=None):
 	
 	if menuType == 3:
 		print('\nForum Responde Ai.\n')
+
+		print('User: '+ str(publicKey)+'\n')
 
 		newsPosts = listPosts(chainName)
 		updatePostList(newsPosts, posts)
@@ -213,7 +331,7 @@ def print_menu(menuType, posts=None, chainName=None, blockedPosts=None):
 			print('Sem posts na Disciplina\n')
 		
 		print('Blocked Posts:\n')
-		blockedPosts = updateBlockedPostList()
+		blockedPosts = updateBlockedPostList(chainName)
 
 		if blockedPosts:
 			for post in blockedPosts:
@@ -228,12 +346,13 @@ def print_menu(menuType, posts=None, chainName=None, blockedPosts=None):
 def act1Menu1():
 	print('Criação de Chave\n')
 
-	passwrd = input('Insira sua senha segura: ')
-	publicKey,privateKey = newKey(passwrd)
+	password = input('Insira sua senha segura: ')
+	#publicKey,privateKey = newKey(password)
+	publicKey,privateKey = '1','2'
 
 	menuType = 2
 	
-	return privateKey, publicKey, menuType
+	return password, privateKey, publicKey, menuType
 
 def act1Menu2():
 	chains = listChain()
@@ -250,8 +369,7 @@ def act2Menu2(publicKey):
 	chain = joinChain(chainName, publicKey)
 
 	menuType = 3
-	print('act'+str(chainName))
-
+	
 	return chain, chainName, menuType
 
 def act3Menu2():
@@ -286,81 +404,103 @@ def act2Menu3(chainName):
 	destPort = input('Insira a porta de execucao de destino: ')
 	sendChainHost(chainName, hostIp, hostPort, destIp, destPort)
 
-def act3Menu3(chainName):
+def act3Menu3(chainName, posts):
 	print('\nVisualizar Post\n')
 
-	post = input('Insira o id do post: ')
+	hash = input('Insira o id do post: ')
 	outputFile = input('Insira o path de saida do arquivo: ')
-	print(getPost(chainName, post, outputFile))
-	print('\nA Reputacao do post é '+getRepsPost(chainName, post))
+	for post in posts:
+		if hash == post.getHash():
+			print(getPost(chainName, hash, outputFile))
+			post.setReputation(getRepsPost(chainName, hash))
+			#print('\nA Reputacao do post é '+str(getRepsPost(chainName, hash)))
+			print('\nA Reputacao do post é '+str(post.getReputation()))
+		else:
+			print('Post não encontrado.')
 
 def act4Menu3(chainName, privateKey, posts):
 	print('\nCriar Post\n')
 
-	nameFile = input('Insira o nome do arquivo: ')
-	pathFile = input('Insira o path do arquivo: ')
+	post = Post()
 
-	#print(newPost(chainName, privateKey, pathFile))
+	post.setFileName(input('Insira o nome do arquivo: '))
+	post.setFilePath = input('Insira o path do arquivo: ')
+	post.setPrivateKey(privateKey)
+	post.setReputation('0')
 	
-	post = (newPost(chainName, privateKey, pathFile), nameFile, privateKey)
+	post.setHash((newPost(chainName, privateKey, post.getFilePath()), post.getFileName(), post.getPrivateKey()))
 	posts.append(post)
 
 
-def act5Menu3(chainName, privateKey):
+def act5Menu3(chainName, privateKey, posts):
 	print('\nCurtir Post\n')
 	
-	post = input('Insira o id do post: ')
+	hash = input('Insira o id do post: ')
 	msg = input('Insira o motivo do like: ')
+	for post in posts:
+		if hash == post.getHash():
+			likePost(chainName, hash, privateKey, msg)
+		else:
+			print('Post não encontrado.')
 
-	likePost(chainName,post,privateKey,msg)
-
-def act6Menu3(chainName, privateKey):
+def act6Menu3(chainName, privateKey, posts):
 	print('\nDescurtir Post\n')
 	
-	post = input('Insira o id do post: ')
+	hash = input('Insira o id do post: ')
 	msg = input('Insira o motivo do dislike: ')
-
-	dislikePost(chainName,post,privateKey,msg)
+	for post in posts:
+		if hash == post.getHash():
+			dislikePost(chainName, hash, privateKey, msg)
+		else:
+			print('Post não encontrado.')
 
 def act7Menu3(chainName, publicKey):
 	print('\nVisualizar Reputacao\n')
-
-	print('A sua reputação na disciplina '+chainName+' é '+getRepsUser(chainName,publicKey))
+	
+	print('A sua reputação na disciplina '+chainName+' é '+getRepsUser(chainName, publicKey))
 
 def act8Menu3():
 	menuType = 2
 	return menuType
 
 def startMenu():
-    global privateKey
-    global publicKey
+    global user
+    #global privateKey
+    #global publicKey
     global menuType
     global exit	
     while menuType == 1:
         print_menu(menuType)
         selection = input("Sua escolha: ")
         if "1" == selection:
-            privateKey, publicKey, menuType = act1Menu1()
+            password, privateKey, publicKey, menuType = act1Menu1()
+            user.setPassword(password)
+            user.setPublicKey(publicKey)
+            user.setPrivateKey(privateKey)
         if "2" == selection:
             exit = True
             return
 
 def chainMenu():
-    global privateKey
-    global publicKey
-    global menuType
+    global user
     global chain
-    global chainName
+    #global privateKey
+    #global publicKey
+    global menuType
+#   global chain
+#    global chainName
     global posts
     global exit
     while menuType == 2:
-        print_menu(menuType)
+        print_menu(menuType, publicKey=user.getPublicKey())
         selection = input("Sua escolha: ")
         if "1" == selection:
             act1Menu2()
         if "2" == selection:
-            chain, chainName, menuType = act2Menu2(publicKey)
-            print(chainName)
+            hash, chainName, menuType = act2Menu2(user.getPublicKey())
+            chain.setName(chainName)
+            chain.setHash(hash)
+            chain.setPionerKey(user.getPublicKey())
         if "3" == selection:
             act3Menu2()
         if "4" == selection:
@@ -370,30 +510,32 @@ def chainMenu():
             return
 
 def postMenu():
-    global privateKey
-    global publicKey
+    global user
+    global chain
+#    global privateKey
+#    global publicKey
     global menuType
-    global chainName
+#    global chainName
     global blockedPosts
     global posts
     global exit
     while menuType == 3:
-        print_menu(menuType, posts, chainName, blockedPosts)
+        print_menu(menuType, posts, chain.getName(), blockedPosts, publicKey=user.getPublicKey())
         selection = input("Sua escolha: ")
         if "1" == selection:
-            act1Menu3(chainName)
+            act1Menu3(chain.getName())
         if "2" == selection:
-            act2Menu3(chainName)
+            act2Menu3(chain.getName())
         if "3" == selection:
-            act3Menu3(chainName)
+            act3Menu3(chain.getName(), posts)
         if "4" == selection:
-            act4Menu3(chainName, privateKey, posts)
+            act4Menu3(chain.getName(), user.getPrivateKey(), posts)
         if "5" == selection:
-            act5Menu3(chainName, privateKey)
+            act5Menu3(chain.getName(), user.getPrivateKey(), posts)
         if "6" == selection:
-            act6Menu3(chainName, privateKey)
+            act6Menu3(chain.getName(), user.getPrivateKey(), posts)
         if "7" == selection:
-            act7Menu3(chainName, publicKey)
+            act7Menu3(chain.getName(), user.getPublicKey())
         if "8" == selection:
             menuType = act8Menu3()
         if "9" == selection:
@@ -406,7 +548,9 @@ if __name__ == "__main__":
 	#dirPath = input('Insira o diretorio de execucao: ')
 	#startHost(hostPort, dirPath)
 	
-	privateKey, publicKey, chain, chainName = '', '', '', ''
+	#privateKey, publicKey, chain, chainName = '', '', '', ''
+	user = User()
+	chain = Chain()
 	posts = []
 	blockedPosts = []
 	exit = False
